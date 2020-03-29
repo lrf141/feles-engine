1f4a658ee20b3e67a17dba754f52a7f5f1774a62
lrf141 <lucky.rabbitfoot141@gmail.com>
2020-03-29 21:52:20 +0900

    Add simple TCP server that using Unix Domain Socket

--- main.go
+++ main.go
@@ -5,28 +5,66 @@ package main
 // #include<feles.h>
 import "C"
 import (
-        "github.com/gorilla/mux"
-        "io"
+        "bufio"
+        "fmt"
+        "io/ioutil"
         "log"
+        "net"
         "net/http"
+        "net/http/httputil"
+        "os"
+        "strings"
 )
 
 func main() {
 
         C.echo()
 
-        srv := mux.NewRouter()
-
-        srv.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-                _, err := io.WriteString(writer, "pong")
-                if err != nil {
-                        writer.WriteHeader(http.StatusInternalServerError)
-                }
-        })
-
-        http.Handle("/", srv)
-
-        if err := http.ListenAndServe(":8080", nil); err != nil {
+        err := os.RemoveAll(unixDomainSockDir)
+        if err != nil {
                 log.Fatal(err.Error())
+        }
+
+        // support unix domain socket only
+        srv, err := net.Listen(protoType, unixDomainSockDir)
+        if err != nil {
+                log.Println("Server Listen Error")
+                log.Fatal(err.Error())
+        }
+
+        defer func() {
+                err := srv.Close()
+                if err != nil {
+                        log.Fatal(err.Error())
+                }
+        }()
+
+        for {
+                conn, err := srv.Accept()
+                if err != nil {
+
+                }
+                go func() {
+                        fmt.Printf("Accept:%v\n", conn.RemoteAddr())
+                        request, _ := http.ReadRequest(bufio.NewReader(conn))
+                        dump,_ := httputil.DumpRequest(request,true)
+                        fmt.Println(string(dump))
+
+                        response := http.Response{
+                                StatusCode:       200,
+                                ProtoMajor:       1,
+                                ProtoMinor:       0,
+                                Body:             ioutil.NopCloser(strings.NewReader("Hello I am Response.")),
+                        }
+                        err := response.Write(conn)
+                        if err != nil {
+                                log.Println(err)
+                                response.StatusCode = http.StatusInternalServerError
+                        }
+                        err = conn.Close()
+                        if err != nil {
+                                log.Println("Connection could not close.")
+                        }
+                }()
         }
 }
